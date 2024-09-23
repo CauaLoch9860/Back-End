a708b2a62bfebf832bd661949bbb5d73967e47ba
Cauã Loch <88676881+CauaLoch9860@users.noreply.github.com>
2024-09-23 18:15:32 -0300

    Add files via upload

---
+++ exercicio00/src/main/java/org/example/Main.java
@@ -0,0 +1,31 @@
+package org.example;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        while(true) {
+            String[] times = {"Criciúma", "Palmeiras", "Flamengo"};
+
+            Scanner leitor = new Scanner(System.in);
+
+            System.out.println("Digite o primeiro nome: ");
+            String primeiroNome = leitor.nextLine();
+
+            if (primeiroNome == "") return;
+
+            System.out.println("Digite o segundo nome: ");
+            String segundoNome = leitor.nextLine();
+
+            Random geradorAleatorio = new Random();
+            int codigoUsuario = geradorAleatorio.nextInt(10, 99);
+
+            int posicaoAleatoria = geradorAleatorio.nextInt(times.length);
+
+            String timeEscolhido = times[posicaoAleatoria];
+
+            System.out.println(primeiroNome + segundoNome + codigoUsuario + timeEscolhido);
+        }
+    }
+}
